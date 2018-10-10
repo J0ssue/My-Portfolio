3da2ad2c5d84a6a9840edcf11aafad423d2f8527
J0ssue <jdiaz66@icloud.com>
2018-10-10 12:44:48 +0100

    re-arranged paragraphs inside about component for better layout

--- src/components/About.js
+++ src/components/About.js
@@ -17,11 +17,11 @@ class About extends Component {
 						<p className="bio-p">
 							I am passionate about building excellent software that improves the lives of those around me. I specialize in JavaScript, HTML, CSS, and React, I have over 1 year experience building projects on my own and have been teaching myself how to program on my free time.
 						</p>
-					</div>
-					<div className="column">
 						<p className="bio-p">
 							I've worked in varied projects which showcase in github in which I've had the chance to experiment with different technologies that enable front end development to be more proficient.All of my work is produced from Portugal. 
 						</p>
+					</div>
+					<div className="column">
 						<p className="bio-p">
 							What would you do if you had a software expert available at your fingertips?
 						</p>
