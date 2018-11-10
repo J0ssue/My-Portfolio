5c323fd06ae8a9f0243e2e44d16b31e47f13ff18
J0ssue <jdiaz66@icloud.com>
2018-11-10 13:47:48 +0000

    added technologies used in each project and removed the description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,35 +12,40 @@ const projects = {
 		title: 'Memory Card Game',
 		desc: 'The goal is to match every card with it\'s corresponding match. Click thorugh each card on the deck and find its match by clicking on another card.',
 		url: 'https://github.com/J0ssue/Memory-Card-Game',
-		live: 'https://j0ssue.github.io/Memory-Card-Game/'
+		live: 'https://j0ssue.github.io/Memory-Card-Game/',
+		icons: ['fab fa-js', 'fab fa-html5', 'fab fa-css3-alt']
 	},
 
 	techDocs: {
 		title: 'ES6 Technical Documentation',
 		desc: 'This technical documentation project holds JavaScript ES6 documentation. HTML rendered with JavaScript using MVC.',
 		url: 'https://github.com/J0ssue/fcc-technical-docs-page',
-		live: 'https://j0ssue.github.io/fcc-technical-docs-page/'
+		live: 'https://j0ssue.github.io/fcc-technical-docs-page/',
+		icons: ['fab fa-js', 'fab fa-html5', 'fab fa-css3-alt'],
 	},
 
 	landing: {
 		title: 'Landing Page',
 		desc: 'This landing page displays a programming course business landing page. To render the information on each section I used the most basic idea of MVC to practice this technique of organizing code.',
 		url: 'https://github.com/J0ssue/fcc-product-landing-page',
-		live: 'https://j0ssue.github.io/fcc-product-landing-page/'
+		live: 'https://j0ssue.github.io/fcc-product-landing-page/',
+		icons: ['fab fa-js', 'fab fa-html5', 'fab fa-css3-alt'],
 	},
 
 	githubProf: {
 		title: 'Github Profile',
 		desc: 'In this small project I use the github API to fetch information about my personal profile and display it onto the DOM.',
 		url: 'https://github.com/J0ssue/github_profile',
-		live: ' https://j0ssue.github.io/github_profile/'
+		live: ' https://j0ssue.github.io/github_profile/',
+		icons: ['fab fa-js', 'fab fa-html5', 'fab fa-css3-alt'],
 	},
 
 	survey: {
 		title: 'Survey Form',
 		desc: 'This is FreeCodeCamp\'s survey form project for the Responsive Web Design Projects.It includes index.html, style.css, background.jpg, app.js, helper.js.',
 		url: 'https://github.com/J0ssue/fcc-survey-form',
-		live: 'https://j0ssue.github.io/fcc-survey-form/'
+		live: 'https://j0ssue.github.io/fcc-survey-form/',
+		icons: ['fab fa-js', 'fab fa-html5', 'fab fa-css3-alt'],
 	},	
 
 };
@@ -57,6 +62,11 @@ class Projects extends React.Component {
 }
 
 const ProjectContainer = () => {
+	const cardGameIcons = projects.cardGame.icons;
+	const techDocsIcons = projects.techDocs.icons;
+	const landingIcons = projects.landing.icons;
+	const githubProfIcons = projects.githubProf.icons;
+	const surveyIcons = projects.survey.icons;
 	return (
 		<div id="project-container">
 				<Project 
@@ -65,6 +75,7 @@ const ProjectContainer = () => {
 					live={projects.cardGame.live} 
 					url={projects.cardGame.url} 
 					img={memoryImg}
+					icons={cardGameIcons}
 				/>
 
 				<Project 
@@ -73,6 +84,7 @@ const ProjectContainer = () => {
 					live={projects.techDocs.live} 
 					url={projects.techDocs.url} 
 					img={techDocsImg}
+					icons={techDocsIcons}
 				/>
 				<Project 
 					title={projects.landing.title} 
@@ -80,6 +92,7 @@ const ProjectContainer = () => {
 					live={projects.landing.live} 
 					url={projects.landing.url} 
 					img={landingImg}
+					icons={landingIcons}
 				/>
 				<Project 
 					title={projects.githubProf.title} 
@@ -87,6 +100,7 @@ const ProjectContainer = () => {
 					live={projects.githubProf.live} 
 					url={projects.githubProf.url} 
 					img={githubProf}
+					icons={githubProfIcons}
 				/>
 				<Project 
 					title={projects.survey.title} 
@@ -94,6 +108,7 @@ const ProjectContainer = () => {
 					live={projects.survey.live} 
 					url={projects.survey.url} 
 					img={surveyImg}
+					icons={surveyIcons}
 				/>
 		</div>	
 	);
@@ -104,7 +119,13 @@ const Project = (props) => {
 		<div className="project">
 			<h3>{props.title}</h3>
 			<img src={props.img} />
-			<p>{props.desc}</p>
+			<div className="icons-container">
+				{props.icons.map((icon, index) => {
+					return (
+						<i className={icon} key={index}></i>
+					);
+				})}	
+			</div>
 			<div className="btns">
 				<em><a href={props.url} target="_blank" className="button">github</a> <a href={props.live} className="button" target="_blank">live</a></em>
 			</div>
