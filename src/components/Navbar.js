602096aade6c4431fdefba4bcc0cb88ecfd206d4
J0ssue <jdiaz66@icloud.com>
2018-11-11 23:21:10 +0000

    added button-glow animation to navbar brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends Component  {
 		return (
 			<nav id="navbar">
 				<div className="nav-container">
-					<div id="nav-brand">
+					<div id="nav-brand" className="button-glow">
 						 <a href="#header">[ <i className="fab fa-react"></i><i className="fab fa-js-square"></i><i className="fab fa-html5"></i><i className="fab fa-css3"></i><i className="fab fa-github-alt"></i>] JDiaz<i className="fas fa-code"></i></a>	
 					</div>
 					<div id="menu-btn">
